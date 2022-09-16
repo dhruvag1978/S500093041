eef06cd6f399d7cbbd25a3ba3d395bcc533ea732
dhruvag1978 <dhruvagarwal1453@gmail.com>
2022-09-16 11:51:34 +0530

    code is edited

--- program.java
+++ program.java
@@ -1,52 +1,48 @@
-class Main
-{
-public static void main(String args[])
-{
-dailyworker d=new dailyworker(254,"Arjun",75);
-salariedworker s=new salariedworker(666,"Unni",100);
-d.compay(45);
-s.compay();
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main (String[] args) {	
+	
+		int n = 4;
+		Triplet arr[] = new Triplet[n];
+
+		arr[0] = new Triplet(1, 2, 3);
+		arr[1] = new Triplet(2, 2, 4);
+		arr[2] = new Triplet(5, 6, 1);
+		arr[3] = new Triplet(10, 2, 10);
+
+		// Sorting the array
+		Arrays.sort(arr);
+		
+		// printing the
+		// Triplet array
+		print(arr);		
+	}
+
+	public static void print(Triplet[] arr){
+		for(int i = 0;i < arr.length;i++){
+			System.out.println(arr[i]);
+		}
+	}
 }
-}
-class worker
-{
-String name;
-int empno;
-worker(int no,String n)
-{ empno=no; name=n; }
-void show()
-{
-System.out.println("\n--------------------------");
-System.out.println("Employee number : "+empno);
-System.out.println("Employee name : "+name);
-}
-}
-class dailyworker extends worker
-{
-int rate;
-dailyworker(int no,String n,int r)
-{
-super(no,n);
-rate=r;
-}
-void compay(int h)
-{
-show();
-System.out.println("Salary : "+rate*h);
-}
-}
-class salariedworker extends worker
-{
-int rate;
-salariedworker(int no,String n,int r)
-{
-super(no,n);
-rate=r;
-}
-int hour=40;
-void compay()
-{
-show();
-System.out.println("Salary : "+rate*hour);
-}
+class Triplet implements Comparable<Triplet> {
+	int x;
+	int y;
+	int z;
+
+	public Triplet(int x,int y,int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public String toString() {
+		return "(" + x + "," + y + "," + z + ")";
+	}
+	
+	// Overriden method to compare
+	// values of the last element.
+	public int compareTo(Triplet a){
+		return this.z - a.z;
+	}
 }
